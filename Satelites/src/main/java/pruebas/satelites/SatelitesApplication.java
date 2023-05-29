cdec3bd9fd0209d2137fe0f745472396f93dfbb0
Pablo Piazza <ppiazza@fi.uba.ar>
2023-05-29 18:42:38 -0300

    Satellite message decoder - ML entry exercise

---
+++ Satelites/src/main/java/pruebas/satelites/SatelitesApplication.java
@@ -0,0 +1,13 @@
+package pruebas.satelites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatelitesApplication.class, args);
+	}
+
+}
